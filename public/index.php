e7de0600a12abba72d9d42c30182ca5a490bcca1
Oliver <OIOl.skp@edu.mercantec.dk>
2023-11-20 13:30:00 +0100

    require routes were changed to the linux standard instead of windows, since it will be hosted on a linux server.

--- public/index.php
+++ public/index.php
@@ -3,15 +3,15 @@ define('ROOT_DIR', dirname(__DIR__));
 define('UPLOAD_DIR', '/uploads');
 define('VIEW_DIR', ROOT_DIR . '/resources/views');
 
-require_once(ROOT_DIR.'\vendor\autoload.php');
+require_once(ROOT_DIR.'/vendor/autoload.php');
 use Pecee\SimpleRouter\SimpleRouter as Route;
 
-require_once(ROOT_DIR.'\routes.php');
+require_once(ROOT_DIR.'/routes.php');
 
 session_start();
 Route::setDefaultNamespace('Controllers');
 
-require_once(ROOT_DIR.'\helpers.php');
+require_once(ROOT_DIR.'/helpers.php');
 
 // Start the routing
 Route::start();
