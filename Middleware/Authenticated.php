29164e9022038334b0c35d8dee93ea52da52bcea
oliv6000 <OIOl.skp@edu.mercantec.dk>
2023-11-30 23:26:34 +0100

    Added functionality for JWT in requests which are locked behind authenticated middleware class
    Currently the post/group routes are outside of the group, but that is only for testing purposes, will change once we know how we need to retrieve the JWT from the client/user
    
    Also added retirect functionality for both page not found and access denied.

--- Middleware/Authenticated.php
+++ Middleware/Authenticated.php
@@ -1,6 +1,15 @@
 <?php
-
 namespace Middleware;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\BeforeValidException;
+use Firebase\JWT\SignatureInvalidException;
+
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(ROOT_DIR);
+$dotenv->load();
 
 use Pecee\Http\Middleware\IMiddleware;
 use Pecee\Http\Request;
@@ -9,7 +18,68 @@ class Authenticated implements IMiddleware
 {
     public function handle(Request $request): void
     {
-        echo 'This user is authenticated, if not, redirect.';
-        die;
+        /*
+            IF Rene has time to change it in the app, to where the jwt response is in the header of the request
+            it needs be changed to $_SERVER['HTTP_AUTHORIZATION'] instead of get
+            since it's more secure, (value wont be logged, and much more)
+        */
+        if (!isset($_GET['jwt'])) {
+            echo json_encode(['status' => 'failed', 'response' => 'no jwt']);
+            die;
+        }
+        $jwt = $_GET['jwt'];
+        
+        try {
+                // This is to make sure that there it's no more than X seconds since the request was made,
+                // if more time than this has passed, it will fail.
+                JWT::$leeway = 30; // $leeway in seconds
+                $decoded = JWT::decode($jwt, new Key($_ENV["JWT_KEY"], 'HS256'));
+                $decoded_array = (array) $decoded;
+
+                /*
+                    The following section is to make sure the same token is not in circulation for too long.
+                    Since a new token is generated every time a user logs in, it should not become a problem,
+                    but for future use, if it needs to be possible to have a user stay logged in,
+                    there has been set an experation date, which when exceeded,
+                    forces the user to login again, or they can't make requests to in this case,
+                    any routes which make use of the Authenticated middleware.
+                */
+                $jwtIsExpired = $decoded_array['jwt_expiration'];
+                $currentDateTime = date("Y-m-d H:i:s");
+                if ($currentDateTime > $jwtIsExpired) {
+                    echo json_encode(['status' => 'failed', 'response' => 'JWToken has expired']);
+                    die;
+                }
+
+            } catch (ExpiredException $e) {
+                // Token is expired. Handle accordingly.
+                http_response_code(401);
+                echo json_encode(['error' => 'Token expired']);
+                die;
+            } catch (BeforeValidException $e) {
+                // Token is not yet valid. Handle accordingly.
+                http_response_code(401);
+                echo json_encode(['error' => 'Token not yet valid']);
+                die;
+            } catch (SignatureInvalidException $e) {
+                // Token signature is invalid, indicating possible tampering.
+                http_response_code(401);
+                echo json_encode(['error' => 'Invalid token signature']);
+                die;
+            } catch (\Throwable $e) {
+                // Catch any Throwable, including DomainException for malformed UTF-8 characters.
+                http_response_code(400);
+                echo json_encode(['error' => 'Malformed or invalid JWT']);
+                die;
+            } catch (\Exception $e) {
+                /*
+                    Catch any Exception, including DomainException for malformed UTF-8 characters.
+                    This is a last case scenario, so if there is some unforseen error with the jwToken
+                    the following will be sent back as a response to the end-user.
+                */
+                http_response_code(400);
+                echo json_encode(['error' => 'Malformed or invalid JWT']);
+                die;
+            }
     }
 }
